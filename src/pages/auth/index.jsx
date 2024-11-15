db305c08dd77a90ee8b2a9e0a3d2115e64d71acb
gokuviku <vikasvikusoni019@gmail.com>
2024-11-15 13:01:56 +0530

    have thi code3 upar push karis

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { TabsTrigger, Tabs, TabsList } from '@radix-ui/react-tabs';
+import { GraduationCap } from 'lucide-react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const AuthPage = () => {
-  return (
-    <div>
-      im auth page.
+
+  const [activeTab, setActiveTab] = useState('signin');
+  function handleTabChange(value) {
+    setActiveTab(value);
+  }
+
+  return <div className='flex flex-col min-h-screen'>
+    <header className='px-4 lg:px-6 h-14 flex items-center border-b' >
+      <Link to={'/'} className='flex items-center justify-center' >
+        <GraduationCap className='h-8 w-8 mr-4' />
+        <span className='font-extrabold text-xl' >LMS LEARN</span>
+      </Link>
+    </header>
+
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <Tabs value={activeTab}
+        defaultValue="signin"
+        onvalueChange={handleTabChange}
+        className="w-full max-w-md"
+      >
+        <TabsList className="grid w-full grid-cols-2 ">
+          <TabsTrigger value='signin' >Sign In</TabsTrigger>
+          <TabsTrigger value='signup' >Sign Up</TabsTrigger>
+        </TabsList>
+
+      </Tabs>
     </div>
-  )
+  </div>
+
 }
 
 export default AuthPage
